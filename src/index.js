3a26b671a92e887fc51c80898588de95ab062483
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-04-20 09:19:10 +0530

    Redo and reformat everything

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import {convertWhileStatement, convertAssignExpression} from './convert';
+import {convertWhileStatement, convertAssignExpression, convertSingle} from './convert';
 //import while_loop from '../input_examples/while_loop.json';
-import while_loop from '../examples_output/while_loop.json';
+import while_loop from '../input_examples/while_loop.json';
 import assign_expr from '../input_examples/assign_expr.json';
 import for_loop from '../input_examples/for_loop.json';
 import if_else from '../input_examples/if_else.json';
+import expression from '../input_examples/expression.json';
 import {getChartConfig} from './visualize'
 
-
 //Trying to convert the output into a json file
-  /* var a=convertWhileStatement(while_loop);
+/* var a=convertWhileStatement(while_loop);
    console.log(a);
  var callback = require('node-callback');
     var json=JSON.stringify(a);
@@ -21,8 +21,8 @@ import {getChartConfig} from './visualize'
         };
         console.log("File has been created");
     });*/
-
-
-   var chart_config = getChartConfig(while_loop);
-    console.log(chart_config);
-    new Treant(chart_config);
+let output = convertSingle(while_loop);
+//let output = while_loop;
+var chart_config = getChartConfig(output);
+console.log(JSON.stringify(chart_config, null,  2));
+new Treant(chart_config);
