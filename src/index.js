88f0b79c84f4233773ae8279a14e63127861913e
Sumeet Padavala <sumeet.padavala@gmail.com>
2018-04-26 22:26:52 +0530

    Overhaul UI

--- src/index.js
+++ src/index.js
@@ -9,37 +9,35 @@ import expression from '../input_examples/expression.json';
 import {getChartConfig, getIntermediateChartConfig} from './visualize';
 import {convertNodeObject} from './convertIC';
 import {cleanUp} from './intercleanup';
+import {cleanupGenerators} from './utils.js';
 
-//Trying to convert the output into a json file
-/* var a=convertWhileStatement(while_loop);
-   console.log(a);
- var callback = require('node-callback');
-    var json=JSON.stringify(a);
-    console.log(json);
-    var fs=require("file-system");
-    fs.writeFile('../examples_output/while_output.json', json, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        };
-        console.log("File has been created");
-    });*/
+function display(input_json){
+    cleanupGenerators();
+    $("#basic-example").innerHTML = "";
+    $("#output").innerHTML = "";
+    $("#icg-pane").innerHTML = "";
+    let output = convertAll(input_json);
+    // console.log(JSON.stringify(output, null, 2))
+    let cleanInter = cleanUp(output);
+    console.log(JSON.stringify(cleanInter, null, 2))
+    let icout = convertNodeObject(cleanInter);
+    console.log(icout);
+    //let output = while_loop;
+    var chart_config = getChartConfig(input_json);
+    new Treant(chart_config);
+    console.log("output");
+    console.log(output);
+    var ir_chart_config = getIntermediateChartConfig(output);
+    new Treant(ir_chart_config);
 
-let input_json = for_loop;
+    document.getElementById('icg-pane').innerText = icout;
+    document.getElementById('icg-pane').setAttribute("align","center");
+}
+display(do_while);
 
-let output = convertAll(input_json);
-// console.log(JSON.stringify(output, null, 2))
-let cleanInter = cleanUp(output);
-console.log(JSON.stringify(cleanInter, null, 2))
-let icout = convertNodeObject(cleanInter);
-console.log(icout);
-//let output = while_loop;
-var chart_config = getChartConfig(input_json);
-new Treant(chart_config);
-console.log("output");
-console.log(output);
-var ir_chart_config = getIntermediateChartConfig(output);
-new Treant(ir_chart_config);
-
-document.getElementById('icg-pane').innerText = icout;
-document.getElementById('icg-pane').setAttribute("align","center");
+$("#for").on('click', () => display(for_loop));
+$("#while").on('click', () => display(while_loop));
+$("#do_while").on('click', () => display(do_while));
+$("#if_else").on('click', () => display(if_else));
+$("#expression").on('click', () => display(expression));
+$("#assign_expr").on('click', () => display(assign_expr));
--- src/utils.js
+++ src/utils.js
@@ -1,27 +1,32 @@
 var labelCount = 0;
 var tempCount = 0;
+
+export function cleanupGenerators() {
+    labelCount = 0;
+    tempCount = 0;
+}
 /**
  *  Create a new label
  *  @param  name Custom label name, optional
  *  @return      A string
  */
-function GenLabel(name) {
+export function GenLabel(name) {
     if (name == null)
         return `L${labelCount++}`;
     else
         return name;
     }
 
-function genTemp(name) {
+export function genTemp(name) {
     let temp = {
         type: "Identifier",
         name: null
     }
     if (name == null)
-        temp.name =`t${tempCount++}`;
+        temp.name = `t${tempCount++}`;
     else
         temp.name = name;
-    return temp;    
+    return temp;
 }
 
 function gen(...args) {
@@ -36,6 +41,3 @@ function gen(...args) {
     );
     return args.join("");
 }
-
-exports.genTemp = genTemp;
-exports.GenLabel = GenLabel;
